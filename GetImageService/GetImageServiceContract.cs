598caad611399882fcbb679a149c1df1fefb4853
Astmus <astmusresist@gmail.com>
2017-12-19 15:30:59 +0300

    Basic skeleton of GetImageService

---
+++ GetImageService/GetImageServiceContract.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel.Web;
+
+namespace GetImageService
+{
+    [ServiceContract]    
+    interface GetImageServiceContract
+    {
+        [WebGet(UriTemplate = "img={name}")]
+        Stream GetImage(string name);
+    }
+}
---
+++ GetImageService/ImageTransmitService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetImageService
+{    
+    class ImageTransmitService : GetImageServiceContract
+    {
+        public Stream GetImage(string name)
+        {
+            WebOperationContext.Current.OutgoingResponse.ContentType = "image/jpeg";
+            return File.Open(@"d:\Images&Video\2013 Other\" + name, FileMode.Open);
+        }
+    }
+}
