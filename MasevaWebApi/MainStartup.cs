79df35b4ff48b862b23d66d2e7487fc6a1beb92a
astmus <astmus@live.com>
2020-01-31 22:49:25 +0200

    Project for test web api added

---
+++ MasevaWebApi/MainStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MasevaWebApi.MainStartup))]
+
+namespace MasevaWebApi
+{
+	public class MainStartup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+			app.Run(context =>
+			{
+				context.Response.ContentType = "text/plain";
+				return context.Response.WriteAsync("Hello, world.");
+			});
+		}
+	}
+}
