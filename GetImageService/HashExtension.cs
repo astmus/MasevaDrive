c9974d9f5bd853bf1c0cf092537b3c63d647e3f6
astmus <astmus@live.com>
2020-01-31 22:48:04 +0200

    Get image service parts added

---
+++ GetImageService/HashExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetImageService
+{
+	static class HashExtension
+	{
+		private static MD5CryptoServiceProvider hasher = new MD5CryptoServiceProvider();
+		public static string ToHash(this string path)
+		{
+			var hashBytes = hasher.ComputeHash(Encoding.UTF8.GetBytes(path));
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < hashBytes.Length; i++)
+				sb.Append(hashBytes[i].ToString("X2"));
+			return sb.ToString();
+		}
+	}
+}
