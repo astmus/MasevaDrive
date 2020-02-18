9924fd4e049b87a56c400b41514a6ffbd3cccfcb
astmus <astmus@live.com>
2020-02-18 12:35:11 +0200

    OWIN service application for web api added

---
+++ MasevaWebApi/Controllers/RootController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MasevaWebApi.Models;
+
+namespace MasevaWebApi.Controllers
+{
+	public class RootController : ApiController
+    {
+		//[HttpGet]
+		[Route("")]
+		public IEnumerable<int> GetValues()
+		{
+			return Enumerable.Range(0, 10);
+		}
+
+		//[HttpGet]
+		/*[Route("items")]
+		public IEnumerable<int> GetItems()
+		{
+			return Enumerable.Range(0, 15);
+		}
+		*/
+		//[HttpGet]
+		[Route("string/{value}/{val2}")]
+		public string GetStringValue(string value, string val2)
+		{
+			return "new string" + value+" "+val2;
+		}
+
+		[Route("book")]
+		public Book GetBook()
+		{
+			return new Book() { Author = "Author", Genre = "Genre", Title = "Title" };
+		}
+	}
+}
--- MasevaWebApi/MainStartup.cs
+++ MasevaWebApi/MainStartup.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.Owin;
 using Owin;
+using System.Web.Http;
 
 [assembly: OwinStartup(typeof(MasevaWebApi.MainStartup))]
 
@@ -11,12 +12,25 @@ namespace MasevaWebApi
 	{
 		public void Configuration(IAppBuilder app)
 		{
+			app.UseErrorPage();
 			// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
-			app.Run(context =>
+			/*app.Run(context =>
 			{
+				if (context.Request.Path.Equals(new PathString("/fail")))
+				{
+					throw new Exception("Random exception");
+				}
 				context.Response.ContentType = "text/plain";
 				return context.Response.WriteAsync("Hello, world.");
-			});
+			});*/
+			var config = new HttpConfiguration();
+			config.MapHttpAttributeRoutes();
+			config.Routes.MapHttpRoute("API Default", "{controller}/{action}");
+			config.Formatters.Remove(config.Formatters.XmlFormatter);
+			config.Formatters.JsonFormatter.UseDataContractJsonSerializer = true;
+
+			app.UseWebApi(config);
+
 		}
 	}
 }
