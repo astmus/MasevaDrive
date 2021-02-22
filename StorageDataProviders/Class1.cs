f1d75ebbfb2afe7abb27a48f53067d6ee916a836
astmus <astmus@live.com>
2021-02-22 07:53:46 +0300

    net472 commented

--- StorageDataProviders/Class1.cs
+++ StorageDataProviders/Class1.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace StorageDataProviders
 {
 	public class Class1
 	{
+		public Class1()
+		{
+			var B = new DbContextOptionsBuilder().UseSqlite("");
+
+			var i = new SQLiteDbContext();
+			
+
+		}
 	}
 }
